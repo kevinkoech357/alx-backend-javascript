f71cd831b5ad7b6558cb7dbdf6c17226cf3a57b0
Kevin Koech <kevinkoech@proton.me>
2024-02-29 10:50:04 +0300

    test if calculateNumber works as expected

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+// Test calculateNumber func
+
+const assert = require("assert");
+const calculateNumber = require("./0-calcul");
+
+describe("calculateNumber", function () {
+  it("should return the sum of rounded numbers", function () {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
